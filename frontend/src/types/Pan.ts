a2c20ce41a4631110bf0500a2206bc3fad1728ba
Nathan Brenton <nathan@nathanbrenton.com>
2026-08-31 01:49:01 -0700

    Complete cookware capacity types

--- frontend/src/types/Pan.ts
+++ frontend/src/types/Pan.ts
@@ -1,7 +1,10 @@
 import type { BaseCookingTool } from "./BaseCookingTool";
+import type { CookwareCapacityUnit } from "./CookwareCapacityUnits";
 import type { PanType } from "./PanTypes";
 
 export type Pan = BaseCookingTool & {
   category: "pan";
   type: PanType;
+  capacity: number;
+  capacityUnit: CookwareCapacityUnit;
 };

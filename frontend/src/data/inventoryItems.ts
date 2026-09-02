71d33535bf4383984eb47dffea3f6694c28436e0
Nathan Brenton <nathan@nathanbrenton.com>
2026-09-02 13:49:55 -0700

    Add inventory data module

---
+++ frontend/src/data/inventoryItems.ts
@@ -0,0 +1,6 @@
+import type { InventoryItem } from "../types/InventoryItem";
+import { oats } from "./ingredients";
+import { pantry } from "./ingredientStorageLocations";
+import { oatsAirtightJar } from "./storageContainers";
+
+export const inventoryItems: InventoryItem[] = [];
